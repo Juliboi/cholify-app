076766f8de41aaccab5c21e362b087f08d415c6b
Julian Xu <julian.xu@PRGA-008842.local>
2020-10-06 20:02:06 +0200

    first-commit

---
+++ precache-manifest.b310a90599996dc6c3e57a2d0bc11bfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd166253de567364939767ba6ed0f55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93ee350964ba1b5b04cb",
+    "url": "/static/css/main.4037778b.chunk.css"
+  },
+  {
+    "revision": "0716ec296972dee41bd1",
+    "url": "/static/js/2.15bf1841.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.15bf1841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ee350964ba1b5b04cb",
+    "url": "/static/js/main.f2eda4da.chunk.js"
+  },
+  {
+    "revision": "248d3ef774bdf5a004dd",
+    "url": "/static/js/runtime-main.0401510b.js"
+  },
+  {
+    "revision": "ba622f97c8842323ab7dd2e6b135d8ea",
+    "url": "/static/media/cholify-inline-logo-dark.ba622f97.svg"
+  }
+]);
