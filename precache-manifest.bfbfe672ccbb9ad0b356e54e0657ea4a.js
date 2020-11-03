7feba58ed66bce0ec25c00a205959a900665b016
Julian Xu <julian.xu@avast.com>
2020-11-03 14:40:08 +0100

    next commit

---
+++ precache-manifest.bfbfe672ccbb9ad0b356e54e0657ea4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438b5f2458bee64ac3d12bcab9c16549",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80e84be31c4827d4bc80",
+    "url": "./static/css/main.ee272985.chunk.css"
+  },
+  {
+    "revision": "b1307184249bd1c78aaa",
+    "url": "./static/js/2.72393839.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.72393839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e84be31c4827d4bc80",
+    "url": "./static/js/main.fd82a18b.chunk.js"
+  },
+  {
+    "revision": "8eee173b1c6c1d8ed87eb6a7f454bf5d",
+    "url": "./static/js/main.fd82a18b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6497363f5e478feb7e40",
+    "url": "./static/js/runtime-main.4ffa4d21.js"
+  },
+  {
+    "revision": "ba622f97c8842323ab7dd2e6b135d8ea",
+    "url": "./static/media/cholify-inline-logo-dark.ba622f97.svg"
+  }
+]);
