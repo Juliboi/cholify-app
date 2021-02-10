faa7cb4fc0f6b825c102f55e8205fceaddf02059
Julian Xu <julian.xu@avast.com>
2021-02-10 19:48:10 +0100

    Menu Card tweaks

---
+++ precache-manifest.b3d479c2e70b2e2f88835c9c5229d0d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a23ee1259f1f5da38bf06900b913d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "500a6774c5ad6afbfb35",
+    "url": "./static/css/main.8ab64b2a.chunk.css"
+  },
+  {
+    "revision": "86ab1ea5b4502a013b9b",
+    "url": "./static/js/2.0675bc65.chunk.js"
+  },
+  {
+    "revision": "3f0d459f7371750fd047ea2df883a2f4",
+    "url": "./static/js/2.0675bc65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500a6774c5ad6afbfb35",
+    "url": "./static/js/main.037a3bc9.chunk.js"
+  },
+  {
+    "revision": "6dc1c5ee9b4a1bb07843fc2817e88d94",
+    "url": "./static/js/main.037a3bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6497363f5e478feb7e40",
+    "url": "./static/js/runtime-main.4ffa4d21.js"
+  },
+  {
+    "revision": "ba622f97c8842323ab7dd2e6b135d8ea",
+    "url": "./static/media/cholify-inline-logo-dark.ba622f97.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8c11f9a44b87e89910d847eb534c3dc6.js"
+  "./precache-manifest.b3d479c2e70b2e2f88835c9c5229d0d0.js"
 );
 
 self.addEventListener('message', (event) => {
