625e1873e81c91e6ff670412ba2b73cfb6799c68
Julian Xu <julian.xu@avast.com>
2021-03-11 18:55:31 +0100

    nutrition system dev tweak

---
+++ precache-manifest.d5447f3ca1dd027640b8de4449bc3a98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3134e206c6f0f2f5e85a4d2bfaf926",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d23fe99d91a4174708cb",
+    "url": "./static/css/main.b4b0b521.chunk.css"
+  },
+  {
+    "revision": "db01ccca15e22ad24c46",
+    "url": "./static/js/2.5afcef6d.chunk.js"
+  },
+  {
+    "revision": "3f0d459f7371750fd047ea2df883a2f4",
+    "url": "./static/js/2.5afcef6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23fe99d91a4174708cb",
+    "url": "./static/js/main.b44ae22a.chunk.js"
+  },
+  {
+    "revision": "8d08af659c92ea4574f5d8de80dfba64",
+    "url": "./static/js/main.b44ae22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6497363f5e478feb7e40",
+    "url": "./static/js/runtime-main.4ffa4d21.js"
+  },
+  {
+    "revision": "ba622f97c8842323ab7dd2e6b135d8ea",
+    "url": "./static/media/cholify-inline-logo-dark.ba622f97.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.76cba09a35f15071db312fe0168520d7.js"
+  "./precache-manifest.d5447f3ca1dd027640b8de4449bc3a98.js"
 );
 
 self.addEventListener('message', (event) => {
