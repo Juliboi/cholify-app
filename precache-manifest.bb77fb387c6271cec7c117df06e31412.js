fe84be832c0b169b43749595f6be1806f2268657
Julian Xu <julian.xu@avast.com>
2021-05-08 18:09:53 +0200

    aa

---
+++ precache-manifest.bb77fb387c6271cec7c117df06e31412.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567cf30b5521afea95b85ef9a6bd517c",
+    "url": "./create-recipe/index.html"
+  },
+  {
+    "revision": "c3c2f6b7fe898bf49134",
+    "url": "./create-recipe/static/css/main.a4b6fd4d.chunk.css"
+  },
+  {
+    "revision": "95ea832b3fed0e7b9c22",
+    "url": "./create-recipe/static/js/2.26059f71.chunk.js"
+  },
+  {
+    "revision": "3f0d459f7371750fd047ea2df883a2f4",
+    "url": "./create-recipe/static/js/2.26059f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c2f6b7fe898bf49134",
+    "url": "./create-recipe/static/js/main.34f26b31.chunk.js"
+  },
+  {
+    "revision": "211137b17debff8096db7e65ee9f8d23",
+    "url": "./create-recipe/static/js/main.34f26b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008b2be364a6fad15b78",
+    "url": "./create-recipe/static/js/runtime-main.203d2816.js"
+  },
+  {
+    "revision": "5fb748c00e0fc8372a316dfe242fd2d1",
+    "url": "./create-recipe/static/media/cholify-web-logo.5fb748c0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d7ab0fee8fc27dc249700160d10fd379.js"
+  "./create-recipe/precache-manifest.bb77fb387c6271cec7c117df06e31412.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./create-recipe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
