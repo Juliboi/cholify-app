e998be81535e033430546ab9b75d066ef51dfff5
Julian Xu <julian.xu@avast.com>
2021-07-17 13:53:21 +0200

    recommended section summary

--- precache-manifest.362c320276439c040d7213033567f72e.js
+++ precache-manifest.362c320276439c040d7213033567f72e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bfa5aaccb41e41c316ef31452ac32f00",
+    "revision": "7f3f538b727abb507a4158f1a1155378",
     "url": "./index.html"
   },
   {
-    "revision": "8364f5b25315c01cde3e",
-    "url": "./static/css/main.74f00ae9.chunk.css"
+    "revision": "e8b326a7ada91dd756ef",
+    "url": "./static/css/main.a89f0e1d.chunk.css"
   },
   {
-    "revision": "b8a568f74c35a917a60e",
-    "url": "./static/js/2.9f9b964b.chunk.js"
+    "revision": "0493b0991799ed4c4f1d",
+    "url": "./static/js/2.eb9eb7cf.chunk.js"
   },
   {
     "revision": "3f0d459f7371750fd047ea2df883a2f4",
-    "url": "./static/js/2.9f9b964b.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.eb9eb7cf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8364f5b25315c01cde3e",
-    "url": "./static/js/main.ba569e69.chunk.js"
+    "revision": "e8b326a7ada91dd756ef",
+    "url": "./static/js/main.6672a589.chunk.js"
   },
   {
     "revision": "a4ad5fbf3da0474fa0b28b4d7709d28b",
-    "url": "./static/js/main.ba569e69.chunk.js.LICENSE.txt"
+    "url": "./static/js/main.6672a589.chunk.js.LICENSE.txt"
   },
   {
     "revision": "6497363f5e478feb7e40",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.310e29b500f202898ca72529e0639cd6.js"
+  "./precache-manifest.362c320276439c040d7213033567f72e.js"
 );
 
 self.addEventListener('message', (event) => {
